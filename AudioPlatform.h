5c7073d372b082745696042f09946903891e8199
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-03-19 17:54:56 -0500

    Added more platform-specific files and constants

--- AudioPlatform.h
+++ AudioPlatform.h
@@ -4,8 +4,11 @@
 #include "Arduino.h"
 #include "AudioStream.h"
 #include "arm_math.h"
+#include "SPI.h"
 
 #define DELAY_QUEUE_SIZE  (176512 / AUDIO_BLOCK_SAMPLES)
+#define MAX_BUFFERS 53
+
 #define digitalWriteFast digitalWrite
 
 #endif
